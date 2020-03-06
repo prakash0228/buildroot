de0c95d8252dbb549dcc047ac6f5dbcd3cae79a0
Damian Danylko <d.danylko@metrological.com>
2020-03-06 11:18:54 +0100

    [Explora Plus] Update Netflix hash, use proper version of appbootKey

--- package/netflix52/netflix52.mk
+++ package/netflix52/netflix52.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX52_VERSION = 798f5dbd401b83eaa933ca3d68ca454cc35a2713
+NETFLIX52_VERSION = 95ea2706c1cc5591b86b9042ee451dbe9a7dcc8a
 NETFLIX52_SITE = git@github.com:Metrological/netflix.git
 NETFLIX52_SITE_METHOD = git
 NETFLIX52_LICENSE = PROPRIETARY
